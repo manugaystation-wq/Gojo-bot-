3b8e6564b8019f23d70cdfbf7e095c08b05cfd11
manugaystation-wq <manugaystation@gmail.com>
2026-07-19 14:46:46 +0200

    Update prefixSupport.js

--- src/services/music/prefixSupport.js
+++ src/services/music/prefixSupport.js
@@ -1,10 +1,8 @@
 import { MessageFlags } from 'discord.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
-
-export function getMusicDeferOptions(interaction) {
-    return interaction._isPrefixCommand ? {} : { flags: MessageFlags.Ephemeral };
+export function getMusicDeferOptions() {
+    return {};
 }
-
 export async function deferMusicCommand(interaction) {
     return InteractionHelper.safeDefer(interaction, getMusicDeferOptions(interaction));
 }
