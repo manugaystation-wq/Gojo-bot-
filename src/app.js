7576fe3ddb2ab9e22c30c10c75e1b4dd0c0560f0
manugaystation-wq <manugaystation@gmail.com>
2026-08-04 20:30:05 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ import { loadCommands, registerCommands as registerSlashCommands } from './handl
 import { runSafeTask, handleTaskError, ErrorCodes } from './utils/errorHandler.js';
 import { initializeMusic } from './services/music/riffySetup.js';
 import { shutdownMusic } from './services/music/playerHandler.js';
+import { sweepExpiredDeletes } from './services/moderation/deleteService.js';
 import pkg from '../package.json' with { type: 'json' };
 import { EXPECTED_SCHEMA_VERSION, EXPECTED_SCHEMA_LABEL } from './config/database/schemaVersion.js';
 
@@ -89,6 +90,10 @@ class TitanBot extends Client {
       startupLog('Logging into Discord...');
       await this.login(this.config.bot.token);
       startupLog('Discord login successful');
+
+      await sweepExpiredDeletes(this).catch((error) => {
+        logger.error('Failed to sweep expired /delete entries on startup:', error);
+      });
       
       startupLog('Registering slash commands globally...');
       await this.registerCommands();
